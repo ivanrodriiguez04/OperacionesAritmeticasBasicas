9757a0f1148fc1af1167f5db692f5c67052ba8ce
DESKTOP-R294T53\irm <ivanrodriiguez04@gmail.com>
2023-02-01 10:50:14 +0100

    operaciones

---
+++ operacionesAritmeticasBasicas/src/main/webapp/script/operacionesAritmeticas.js
@@ -0,0 +1,22 @@
+function ejer1(){
+	resultado=10.25+0.75;
+	document.write(typeof(resultado));
+}
+
+function ejer2(){
+	let numero1= 23;
+	let numero2=34;
+	
+	let resultadoOperacion = numero1 + numero2;
+	document.write(resultadoOperacion)
+}
+
+function ejer3(){
+	let operacion=46+8*(2/2)+46;
+	document.write(operacion);
+}
+
+function ejer4(){
+	let operacion=46+8*2/(2+46);
+	document.write(operacion);
+}
